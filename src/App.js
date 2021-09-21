36e76ed6fee030563fa32b92975957ba648e1daa
marco-ih-bcn <marco.santonastasi@gmail.com>
2021-09-21 12:39:46 +0200

    part_4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FilmListing } from './components/FilmListing';
 import { FilmDetail } from './components/FilmDetail';
 import TMDB from './API/TMDB';
+import axios from 'axios'
 
 function App() {
 	const [films, setFilms] = useState(TMDB.films);
@@ -9,10 +10,9 @@ function App() {
 	const [current, setCurrent] = useState({});
 
 	const handleFaveToggle = (film) => {
-		// film is a film object from the films array, passed when add/remove film icon is clicked
-		let newFaves = faves.slice(); // copy the state into a new variable
+		let newFaves = faves.slice();
 		const filmIndex = newFaves.indexOf(film);
-		// when indexOf returns -1 that means the element does not exist in the array
+
 		if (filmIndex === -1) {
 			console.log(`Adding ${film.title} to faves...`);
 			newFaves.push(film);
@@ -25,7 +25,11 @@ function App() {
 
 	const handleDetailsClick = (film) => {
 		console.log('Fetching details for ' + film);
-		setCurrent(film);
+		const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
+		axios.get(url).then(response => {
+			console.log(response)
+			setCurrent(response.data);
+		})
 	};
 
 	return (
